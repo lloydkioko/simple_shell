da017e2c89bff60a8bc93a33ed9a40d1812b17dd
lloydkioko <kiokolloyd@gmail.com>
2022-12-21 02:31:42 -0800

    update

--- shell.h
+++ shell.h
@@ -1,62 +1,47 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
 
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
 
-/* Libraries included */
-
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <signal.h>
 
-extern char **environ;
 
-/**
- * struct builtin_cmd - struct type defining a functiomn pointer to a command
- * @command: command to execute
- * @func: A function pointer to a function that executes
- *         a builtin command.
- */
-
-typedef struct builtin_cmd
-{
-	char *command;
-	int (*func)(char **cmd, int counter, char *argv);
-} built_ins;
-
-/* String manipulation functions */
-char *_strtok(char *str, const char *delim);
-unsigned int check_delim(char c, const char *str);
-
-char *_strcpy(char *dest, char *src);
-char *_strcat(char *dest, char *src);
-int _strcmp(char *s1, char *s2);
-char *_strncpy(char *dest, char *src, int n);
+/* string manipulations functions */
 int _strlen(char *s);
-/*int _strncmp(char *s1, char *s2, int n);*/
-int _strncmp(const char *s1, const char *s2, size_t n);
+char *_strcpy(char *dest, char *src);
+char *_strncat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
 char *_strdup(char *str);
-char *_strtok(char *str, const char *delim);
-char *_memset(char *s, char b, unsigned int n);
+void print_string(char *string);
+char *_memcpy(char *dest, char *Src, unsigned int n);
 
-char *_getenv(char *name);
-void path_finder(char **cmd);
-int _env(char **cmd, int counter, char *argv);
-int exec_builtin(char **command, int counter, char *argv);
-int check_cmd(char *command);
-int clear(char **cmd, int counter, char *argv);
-int __exit(char **cmd, int counter, char *argv);
-int _isdigit(char c);
-int _atoi(char *s);
-char *_itoa(unsigned int n);
-void exec(char **cmd, int counter, char **argv);
-char **parser(char *buffer);
-void printE(int counter, char *cmd, char *argv);
-void execute(char **array, int counter, char **argv, char *buffer);
-char *_getline();
-void hashtag_handle(char *buff);
-char *build(char *token, char *value);
-#endif
+/* shell basic process */
+int main(int argc, char *argv[], char *env[]);
+int shell_loop(char **env);
+char *read_command(char **env);
+char **split_command(char *string);
+void forkwaitexec(int status, char **argv, int *count, int *stad_exit);
+void rm_new_line(char *string);
+int _path(char *args, char **argv, char **env, int *stad_exit);
+char *print_path(char *der, char *args);
+char **_parser(char *string);
+int printenv(char **env, int *stad_exit);
+void built_in(char *string, char **argv, char **env, int *ex_it);
+void simple_print_shell(char *string);
+void print_count(int *count);
+int _putchar(char c);
+int func_ctrl_d(char *string, ssize_t read, int *stad_exit);
+
+/* function signal */
+void sighandler(int sig);
+
+#endif /* _SHELL_H_ */
